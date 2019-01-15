0e248252c5f74f98af1528d8c6636e4f5e17fe40
toshokan <toshokan@shojigate.net>
2019-01-14 23:44:12 -0500

    auto derive serde traits on quix templating structures, test

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,5 +1,17 @@
 use std::fs;
+use std::io::Read;
 
 fn main() -> () {
-    let config = quix::config::Config::new();
+    let config = quix::config::Config::new().unwrap();
+    let mut qxf = config.source.unwrap().clone();
+    qxf.push("quix.json");
+    println!("{:?}", qxf);
+    let mut f = fs::File::open(qxf).unwrap();
+    let mut s = String::new();
+    f.read_to_string(&mut s);
+    println!("{}", s);
+    let qx: quix::templating::QuixFile = serde_json::from_str(&s).unwrap();
+
+    println!("Found a template named {}: {} written by {}", qx.template.name, qx.template.description, qx.template.author);
+    println!("The template has {} variable, {:?}", qx.variables.len(), qx.variables);
 }
