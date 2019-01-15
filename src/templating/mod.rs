ac497c58bff180b252c9bbf58152c12b52907b77
toshokan <toshokan@shojigate.net>
2019-01-15 10:31:58 -0500

    Improve error handling

--- src/templating/mod.rs
+++ src/templating/mod.rs
@@ -1,7 +1,33 @@
+mod error;
+
+use std::io::Read;
+use std::fs;
+use std::path::{Path,PathBuf};
+
+
 #[derive(Deserialize)]
 pub struct QuixFile {
     pub template: Metadata,
     pub variables: Vec<Variable>
+}
+
+impl QuixFile {
+    pub fn open(path: &Path) -> Result<QuixFile, error::Error> {
+        let mut path = PathBuf::from(path);
+        if !path.ends_with("quix.json") {
+            path.push("quix.json")
+        }
+        match fs::File::open(path) {
+            Ok(mut file) => {
+                let mut buf = String::new();
+                if let Err(e) = file.read_to_string(&mut buf) {
+                    return Err(error::Error::IOError(e))
+                }
+                serde_json::from_str(&buf).map_err(|e| error::Error::BadFormat(e))
+            },
+            Err(e) => Err(error::Error::IOError(e)),
+        }
+    }
 }
 
 #[derive(Deserialize)]
