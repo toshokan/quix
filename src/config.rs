1eff935cdd90e797a74b627df9a1228378a94c8d
toshokan <toshokan@shojigate.net>
2019-01-14 23:07:05 -0500

    Load source/dest into config struct

--- src/config.rs
+++ src/config.rs
@@ -8,25 +8,28 @@ pub struct Config {
 
 impl Config {
     pub fn new() -> Result<Config, &'static str> {
-        let _matches = clap::App::new("quix")
+        let matches = clap::App::new("quix")
             .version("0.1.0")
             .author("toshokan <toshokan@shojigate.net")
             .about("a file and directory templating system")
-            .arg(clap::Arg::with_name("source directory")
+            .arg(clap::Arg::with_name("source")
                  .help("The template source directory (containing quix.json)")
                  .short("s")
                  .long("source")
                  .takes_value(true))
-            .arg(clap::Arg::with_name("destination directory")
+            .arg(clap::Arg::with_name("dest")
                  .help("the directory the instantiated template will be copied to")
                  .short("d")
                  .long("destination")
                  .takes_value(true))
             .get_matches();
-        
+
+        let source = matches.value_of("source").map(|source_path| PathBuf::from(source_path));
+        let dest = matches.value_of("dest").map(|source_path| PathBuf::from(source_path));
+
         Ok(Config{
-            source: None,
-            dest: None
+            source,
+            dest
         })
     }
 }
